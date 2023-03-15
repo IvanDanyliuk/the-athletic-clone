c65aff07ed57be3cf758babcfa843aac525baec0
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-03-15 18:27:18 +0200

    Added the NoteForm component

--- src/app/components/adminPanel/forms/creationForms/NoteForm.tsx
+++ src/app/components/adminPanel/forms/creationForms/NoteForm.tsx
@@ -1,10 +1,169 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Box, Button, Grid, styled, Typography } from '@mui/material';
+import dayjs from 'dayjs';
+import { AppDispatch } from '../../../../../features/store';
+import { MaterialModel } from '../../../../models/components';
+import TextInput from '../../../ui/TextInput';
+import ControlledDatePicker from '../../../ui/ControlledDatePicker';
+import TextEditor from '../../ui/TextEditor';
+import { createMaterial, updateMaterial } from '../../../../../features/materials/asyncActions';
+import { selectUser } from '../../../../../features/users/selectors';
+import { uploadImage } from '../../../../services/uploadImage';
+import BackLink from '../../ui/BackLink';
+import SelectField from '../../../ui/SelectField';
+import BackdropLoader from '../../../ui/BackdropLoader';
+import { IMaterial } from '../../../../../features/materials/types';
 
 
-const NoteForm: React.FC = () => {
+
+const Form = styled(Box)`
+  margin-top: 20px;
+`;
+
+const FormRow = styled(Grid)`
+  margin-bottom: 10px;
+`;
+
+const statusOptions = [
+  { label: 'Published', value: 'published' },
+  { label: 'Not Published', value: 'not-published' },
+];
+
+interface INewNoteFormProps {
+  noteToUpdate?: IMaterial
+}
+
+
+const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { register, handleSubmit, control, formState: { errors }, reset } = useForm<MaterialModel>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const user = useSelector(selectUser);
+
+  const handleFormSubmit = async (data: any) => {
+    
+    if(noteToUpdate) {
+      setIsLoading(true);
+      await dispatch(updateMaterial({
+        ...noteToUpdate,
+        title: data.title,
+        image: data.image,
+        publicationDate: data.publicationDate,
+        status: data.status,
+        content: data.content
+      }));
+      setIsLoading(false);
+      navigate('/admin/materials');
+    } else {
+      setIsLoading(true);
+      const imageUrl = data.image.length > 0 ? await uploadImage(data.image[0]) : '';
+      await dispatch(createMaterial({
+        ...data,
+        author: {
+          name: `${user?.firstName} ${user?.lastName}`,
+          photoUrl: user?.userPhotoUrl,
+          organization: user?.organization,
+          position: user?.position
+        },
+        type: 'note',
+        image: imageUrl,
+        publicationDate: dayjs(data.publicationDate).format('DD/MM/YYYY'),
+        content: data.content,
+        views: 0,
+        likes: 0,
+        comments: []
+      }));
+      setIsLoading(false);
+    }
+    reset();
+  };
+
+  useEffect(() => {
+    if(noteToUpdate) {
+      reset({
+        title: noteToUpdate.title,
+        image: noteToUpdate.image,
+        publicationDate: noteToUpdate.publicationDate,
+        status: noteToUpdate.status,
+        content: noteToUpdate.content
+      })
+    }
+  }, []);
+
   return (
-    <div>NoteForm</div>
+    <Box>
+      <BackLink link='/admin/materials' title='Go back' />
+      <Form component='form' onSubmit={handleSubmit(handleFormSubmit)}>
+        <FormRow container>
+          <Grid item xs={12}>
+            <TextInput 
+              name='title' 
+              label='Title'
+              type='text' 
+              register={register}
+              registerOptions={{ required: 'Title is required!' }}
+              error={errors.title}
+            />
+          </Grid>
+        </FormRow>
+        <FormRow container spacing={3}>
+          <Grid item xs={12} md={3}>
+            <TextInput 
+              name='image' 
+              label='Image'
+              type='file'
+              register={register}
+              
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <ControlledDatePicker 
+              name='publicationDate'
+              label='Publication Date'
+              control={control}
+              register={register}
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography>Labels will be here soon!</Typography>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <SelectField 
+              name='status'
+              label='Status' 
+              control={control}
+              register={register}
+              registerOptions={{ required: 'Status is required!' }} 
+              error={errors.status}
+              options={statusOptions}
+            />
+          </Grid>
+        </FormRow>
+        <FormRow container>
+          <Grid item xs={12}>
+            <TextEditor 
+              name='content' 
+              control={control}
+              register={register}
+              error={errors.content}
+            />
+          </Grid>
+        </FormRow>
+        <Button 
+          type='submit'
+          variant='contained'
+        >
+          Submit
+        </Button>
+      </Form>
+      <BackdropLoader open={isLoading} />
+    </Box>
   );
 };
 
-export default NoteForm;
+export default NewNoteForm;
