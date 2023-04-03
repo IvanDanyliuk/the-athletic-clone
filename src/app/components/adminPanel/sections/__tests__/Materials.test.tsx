1bd3530e4005e3f8b191958133db8a7b3b2ff2c4
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-04-03 22:17:16 +0300

    The Materials sub-page has been covered with tests

---
+++ src/app/components/adminPanel/sections/__tests__/Materials.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { store } from '../../../../../features/store';
+import Materials from '../Materials';
+
+
+describe('Materials tests', () => {
+  test('should render the Materials sub-page', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Materials />
+          </LocalizationProvider>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByText(/Materials/)).toBeInTheDocument();
+  });
+});
