cd9517d632dee92480bcffee91540f39527e6e03
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-04-06 13:24:02 +0300

    Added label selecting for the NoteForm component

--- src/app/components/adminPanel/forms/creationForms/NoteForm.tsx
+++ src/app/components/adminPanel/forms/creationForms/NoteForm.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { Box, Button, Grid, styled, Typography } from '@mui/material';
+import { Box, Button, Grid, styled } from '@mui/material';
 import dayjs from 'dayjs';
 import { AppDispatch } from '../../../../../features/store';
 import { MaterialModel, MaterialType } from '../../../../models/components';
@@ -16,6 +16,11 @@ import BackLink from '../../ui/BackLink';
 import SelectField from '../../../ui/SelectField';
 import BackdropLoader from '../../../ui/BackdropLoader';
 import { IMaterial } from '../../../../../features/materials/types';
+import { selectClubsByCountry } from '../../../../../features/clubs/selectors';
+import { selectAllCompetitions } from '../../../../../features/competitions/selectors';
+import { getClubsByCountry } from '../../../../../features/clubs/asyncActions';
+import { getAllCompetitions } from '../../../../../features/competitions/asyncActions';
+import LabelSelect from '../../../ui/LabelSelect';
 
 
 
@@ -42,20 +47,34 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
   const navigate = useNavigate();
   const { register, handleSubmit, control, formState: { errors }, reset } = useForm<MaterialModel>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
 
   const user = useSelector(selectUser);
+  const clubsData = useSelector(selectClubsByCountry);
+  const clubs = clubsData.map(club => ({ label: club.commonName, value: club.commonName }));
+  const competitionsData = useSelector(selectAllCompetitions);
+  const competitions = competitionsData.map(competition => ({ label: competition.fullName, value: competition.fullName }));
+
+  const handleLabelSelect = (event: any) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedLabels(
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
 
   const handleFormSubmit = async (data: any) => {
-    
     if(noteToUpdate) {
       setIsLoading(true);
       await dispatch(updateMaterial({
         ...noteToUpdate,
         title: data.title,
         image: data.image,
-        publicationDate: data.publicationDate,
+        publicationDate: dayjs(data.publicationDate).add(1, 'day').toISOString(),
         status: data.status,
-        content: data.content
+        content: data.content,
+        labels: selectedLabels,
       }));
       setIsLoading(false);
       navigate('/admin/materials');
@@ -72,10 +91,11 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
         },
         type: MaterialType.note,
         image: imageUrl,
-        publicationDate: dayjs(data.publicationDate).format('DD/MM/YYYY'),
+        publicationDate: dayjs(data.publicationDate).add(1, 'day').toISOString(),
         content: data.content,
         views: 0,
         likes: 0,
+        labels: selectedLabels,
         comments: []
       }));
       setIsLoading(false);
@@ -84,14 +104,17 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
   };
 
   useEffect(() => {
+    dispatch(getClubsByCountry('International'));
+    dispatch(getAllCompetitions());
     if(noteToUpdate) {
       reset({
         title: noteToUpdate.title,
         image: noteToUpdate.image,
-        publicationDate: noteToUpdate.publicationDate,
+        publicationDate: dayjs(noteToUpdate.publicationDate).subtract(1, 'day'),
         status: noteToUpdate.status,
         content: noteToUpdate.content
-      })
+      });
+      setSelectedLabels(noteToUpdate.labels);
     }
   }, []);
 
@@ -99,8 +122,8 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
     <Box>
       <BackLink link='/admin/materials' title='Go back' />
       <Form component='form' onSubmit={handleSubmit(handleFormSubmit)}>
-        <FormRow container>
-          <Grid item xs={12}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={9}>
             <TextInput 
               name='title' 
               label='Title'
@@ -110,8 +133,6 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
               error={errors.title}
             />
           </Grid>
-        </FormRow>
-        <FormRow container spacing={3}>
           <Grid item xs={12} md={3}>
             <TextInput 
               name='image' 
@@ -122,15 +143,28 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
             />
           </Grid>
           <Grid item xs={12} md={3}>
+            <LabelSelect 
+              name='Competition Label' 
+              data={competitions} 
+              checkedLabels={selectedLabels} 
+              setLabels={handleLabelSelect} 
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <LabelSelect 
+              name='Clubs Label' 
+              data={clubs} 
+              checkedLabels={selectedLabels} 
+              setLabels={handleLabelSelect} 
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
             <ControlledDatePicker 
               name='publicationDate'
               label='Publication Date'
               control={control}
               register={register}
             />
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Typography>Labels will be here soon!</Typography>
           </Grid>
           <Grid item xs={12} md={3}>
             <SelectField 
@@ -144,8 +178,6 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
               options={statusOptions}
             />
           </Grid>
-        </FormRow>
-        <FormRow container>
           <Grid item xs={12}>
             <TextEditor 
               name='content' 
@@ -154,13 +186,15 @@ const NewNoteForm: React.FC<INewNoteFormProps> = ({ noteToUpdate }) => {
               error={errors.content}
             />
           </Grid>
-        </FormRow>
-        <Button 
-          type='submit'
-          variant='contained'
-        >
-          Submit
-        </Button>
+          <Grid item xs={12} md={2}>
+          <Button 
+            type='submit'
+            variant='contained'
+          >
+            Submit
+          </Button>
+          </Grid>
+        </Grid>
       </Form>
       <BackdropLoader open={isLoading} />
     </Box>
