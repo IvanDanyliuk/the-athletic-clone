f6aad271588085b832472b581ba9b0740a637f0a
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-04-13 19:23:53 +0300

    Added tests for the MaterialsTable component

--- src/app/utils/testing/testDataMocks/materials.ts
+++ src/app/utils/testing/testDataMocks/materials.ts
@@ -30,9 +30,225 @@ export const materialsStateSuccessMock: IMaterialsState = {
         comments: [],
         createdAt: '2023-04-05T14:34:56.462Z',
         updatedAt: '2023-04-05T14:35:57.574Z',
-      }
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8711d4be15abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8710d4be13abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8780d4be15abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8710d4be15abd18e99ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8710d4be17abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8715d4be15abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '643d8710d4be15abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '647d8710d4be15abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
+      {
+        author: {
+          name: 'John Doe',
+          photoUrl: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/surprised-cat-eric-hacke.jpg',
+          organization: 'The Athletic',
+          position: 'Website Administrator'
+        },
+        _id: '642d8719d4be15abd18e94ad',
+        type: 'article',
+        title: 'aaaaaaaaaaaaaaaaaa',
+        content: '<p>sdfsdfsdfsdfsdfsdfsadf</p>',
+        image: '',
+        status: 'published',
+        publicationDate: '2023-04-05T21:00:00.000Z',
+        views: 0,
+        likes: 0,
+        labels: [
+          'Premier League',
+          'Arsenal'
+        ],
+        comments: [],
+        createdAt: '2023-04-05T14:34:56.462Z',
+        updatedAt: '2023-04-05T14:35:57.574Z',
+      },
     ],
-    materialsCount: 1
+    materialsCount: 12
   },
   filters: {
     author: 'John Doe',
