62043f4bdecdc05d0e4f17506e506d5a50b1fad4
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-04-13 19:14:16 +0300

    Added tests for the CompetitionsTableHead component

---
+++ src/app/components/adminPanel/tables/CompetitionsTable/__tests__/CompetitionsTableHead.test.tsx
@@ -0,0 +1,38 @@
+import { screen, fireEvent, cleanup } from '@testing-library/react';
+import { renderWithProviders } from '../../../../../utils/testing/customRenderMethod'; 
+import { setupCompetitionsSuccessHandlers } from '../../../../../utils/testing/serverMocks/competitions';
+import CompetitionsTableHead from '../CompetitionsTableHead';
+import { Order } from '../../../../../../features/competitions/types';
+
+
+const activeCellMock = {
+  title: 'Country', 
+  isSortable: true,
+  sortKey: 'country',
+  order: Order.asc
+};
+
+const onSortMock = jest.fn();
+
+describe('CompetitionsTableHead tests', () => {
+  beforeEach(() => {
+    setupCompetitionsSuccessHandlers();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should render the component', () => {
+    renderWithProviders(
+      <CompetitionsTableHead 
+        activeCell={activeCellMock}
+        onSort={onSortMock}
+      />
+    );
+
+    const sortBtns = screen.getAllByTestId('ArrowDownwardIcon');
+    fireEvent.click(sortBtns[0]);
+    expect(onSortMock).toHaveBeenCalled();
+  });
+});
