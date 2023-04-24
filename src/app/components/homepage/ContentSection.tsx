decfb4b797b0f9c689e73cd13df52d6124b44567
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-04-24 21:37:29 +0300

    Added a basic layout for the content sections

--- src/app/components/homepage/ContentSection.tsx
+++ src/app/components/homepage/ContentSection.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import sc from 'styled-components';
-import { IContentSection } from '../../../features/content/types';
-import { Box, Grid, Typography, styled } from '@mui/material';
+import { Avatar, Box, Grid, Typography, styled } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { v4 as uuid } from 'uuid';
+import { IContentSection } from '../../../features/content/types';
 import MaterialSecondaryInfo from './MaterialSecondaryInfo';
 
 
@@ -11,11 +12,17 @@ interface IContentSectionProps {
   data: IContentSection
 }
 
+const SectionTitle = styled(Typography)`
+  margin-bottom: .8em;
+  font-family: 'Sanchez', serif;
+  font-size: 1.7em;
+`;
+
 const Section = styled(Box)`
   background: #ffffff;
 `;
 
-const Image = sc.img`
+const TopImage = sc.img`
   width: 100%;
 `;
 
@@ -32,6 +39,12 @@ const TopMaterialPreviewText = styled(Typography)`
   line-height: .5em;
 `;
 
+const SecondaryMaterialTitle = styled(Typography)`
+  font-family: 'Crimson Pro', serif;
+  font-size: 1em;
+`;
+
+
 const ContentSection: React.FC<IContentSectionProps> = ({ data }) => {
   if(!data) {
     return <div>Loading...</div>
@@ -39,25 +52,44 @@ const ContentSection: React.FC<IContentSectionProps> = ({ data }) => {
 
   return (
     <Section>
-      <Grid container>
+      <SectionTitle variant='inherit'>{data.name}</SectionTitle>
+      <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          <Box>
-            <Image 
-              src={data.materials[0].image} 
-              alt={data.materials[0]._id} 
-            />
-            <TopMaterialTitle>{data.materials[0].title}</TopMaterialTitle>
-            <TopMaterialPreviewText>
-              {data.materials[0].content.split('.')[0].replace(/(<([^>]+)>)/ig, '')}
-            </TopMaterialPreviewText>
-            <MaterialSecondaryInfo 
-              author={data.materials[0].author.name} 
-              views={data.materials[0].views} 
-            />
-          </Box>
+          <TopImage 
+            src={data.materials[0].image} 
+            alt={data.materials[0]._id} 
+          />
+          <TopMaterialTitle>{data.materials[0].title}</TopMaterialTitle>
+          <TopMaterialPreviewText>
+            {data.materials[0].content.split('.')[0].replace(/(<([^>]+)>)/ig, '')}
+          </TopMaterialPreviewText>
+          <MaterialSecondaryInfo 
+            author={data.materials[0].author.name} 
+            views={data.materials[0].views} 
+          />
         </Grid>
         <Grid item xs={12} md={6}>
-          
+          {data.materials.slice(1).map(material => (
+            <Grid container spacing={3} key={uuid()}>
+              <Grid item xs={9}>
+                <SecondaryMaterialTitle variant='inherit'>
+                  {material.title}
+                </SecondaryMaterialTitle>
+                <MaterialSecondaryInfo 
+                  author={material.author.name} 
+                  views={material.views} 
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Avatar 
+                  src={material.image} 
+                  alt={material._id} 
+                  variant='square' 
+                  sx={{ width: 76, height: 76 }}
+                />
+              </Grid>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Section>
