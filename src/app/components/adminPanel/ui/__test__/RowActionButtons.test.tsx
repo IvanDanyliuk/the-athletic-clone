3968cdb767e1d1dc99c6a3d1fe31f824f21925d1
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-05-02 14:11:38 +0300

    Removed unused imports

--- src/app/components/adminPanel/ui/__test__/RowActionButtons.test.tsx
+++ src/app/components/adminPanel/ui/__test__/RowActionButtons.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import RowActionButtons, { EssenseType } from '../RowActionButtons';
 
