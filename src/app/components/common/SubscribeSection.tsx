fff1ebf35e890c7d9c84d2ca9317f48a2e27bb41
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-05-05 13:59:32 +0300

    Implemented the SubsribeSection component

--- src/app/components/common/SubscribeSection.tsx
+++ src/app/components/common/SubscribeSection.tsx
@@ -5,21 +5,51 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 
-const SubscriptionLink = styled(Link)`
+const SectionContainer = styled(Box)`
+  position: relative;
+  padding: 3em 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #121212;
 
+  h2 {
+    margin-bottom: .5em;
+    width: 25%;
+    font-size: 2.5em;
+    text-align: center;
+    color: #ffffff;
+  }
+`;
+
+const SubscriptionLink = styled(Link)`
+  text-transform: uppercase;
+  text-decoration: none;
+  font-size: 1.2em;
+  font-weight: 500;
+  color: #ffffff;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  svg {
+    margin-left: .5em;
+  }
 `;
 
 const SubscribeSection: React.FC = () => {
   return (
-    <Box>
-      <Typography>
+    <SectionContainer>
+      <Typography variant='h2'>
         Access sports reporting that sets the standard.
       </Typography>
       <SubscriptionLink to='/subscribe'>
         Subscribe for $2
         <FontAwesomeIcon icon={faAngleRight} />
       </SubscriptionLink>
-    </Box>
+    </SectionContainer>
   );
 };
 
