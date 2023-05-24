71d7cdd8b7ceb9af813ffcc41979e1c4af6975f8
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-05-24 13:14:48 +0300

    Added tests for the PostCard component

---
+++ src/app/components/materials/__tests__/PostCard.test.tsx
@@ -0,0 +1,46 @@
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
+import { renderWithProviders } from '../../../utils/testing/customRenderMethod';
+import { postToUpdate } from '../../../utils/testing/testDataMocks/materials';
+import { userToUpdate } from '../../../utils/testing/testDataMocks/users';
+import PostCard from '../PostCard';
+import { IMaterial } from '../../../../features/materials/types';
+
+
+const mockedUseDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+   ...jest.requireActual('react-redux') as any,
+  useDispatch: () => mockedUseDispatch,
+}));
+
+const likedPostMock: IMaterial = {
+  ...postToUpdate,
+likes: [userToUpdate._id!]
+}
+
+
+describe('PostCard tests', () => {
+  test('should call the dispatch function by clicking the like button if the post had been liked', async () => {
+    renderWithProviders(<PostCard post={postToUpdate} user={userToUpdate} />);
+
+    const likeBtn = screen.getByTestId('ThumbUpOutlinedIcon');
+    fireEvent.click(likeBtn);
+
+    await waitFor(() => {
+      expect(mockedUseDispatch).toHaveBeenCalled();
+    });
+  });
+
+  test('should call the dispatch function by clicking the like button if the post had not been liked', async () => {
+    renderWithProviders(
+      <PostCard post={likedPostMock} user={userToUpdate} />,
+    );
+
+    const likeBtn = screen.getAllByTestId('ThumbUpOutlinedIcon');
+    fireEvent.click(likeBtn[1]);
+    
+    await waitFor(() => {
+      expect(mockedUseDispatch).toHaveBeenCalled();
+    });
+  });
+});
