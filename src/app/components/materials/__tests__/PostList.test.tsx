b1e9d362c26107fb0c137fa49bf2231f27dcf25d
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-05-24 14:05:41 +0300

    Added tests for the PostList component

---
+++ src/app/components/materials/__tests__/PostList.test.tsx
@@ -0,0 +1,46 @@
+import { cleanup, screen } from '@testing-library/react';
+import { setupMaterialsSuccessHandlers } from '../../../utils/testing/serverMocks/materials';
+import { renderWithProviders } from '../../../utils/testing/customRenderMethod';
+import { materialsStateSuccessMock, postToUpdate } from '../../../utils/testing/testDataMocks/materials';
+import PostList from '../PostList';
+
+
+describe('PostList tests', () => {
+  beforeEach(() => {
+    setupMaterialsSuccessHandlers();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should render the post data if searched posts exist', async () => {
+    renderWithProviders(
+      <PostList />,
+      {
+        preloadedState: {
+          materials: {
+            ...materialsStateSuccessMock,
+            search: [postToUpdate]
+          }
+        }
+      }
+    );
+    expect(screen.getByText(postToUpdate.author.name)).toBeInTheDocument();
+  });
+
+  test('should render the the message if searched does not posts exist', async () => {
+    renderWithProviders(
+      <PostList />,
+      {
+        preloadedState: {
+          materials: {
+            ...materialsStateSuccessMock,
+            search: null
+          }
+        }
+      }
+    );
+    expect(screen.getByText('Cannot find materials related to your request.')).toBeInTheDocument();
+  });
+});
