10e5e910d532ab6b2807541e59402a532768e666
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-05-28 22:29:25 +0300

    Fixed styles

--- src/app/components/competitions/MatchweekPicker.tsx
+++ src/app/components/competitions/MatchweekPicker.tsx
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
-import { Box, Button, Dialog, Grid, Icon, IconButton, Typography } from '@mui/material';
+import { Box, Button, Dialog, Grid, Icon, IconButton, Typography, styled } from '@mui/material';
 import { CalendarMonth } from '@mui/icons-material';
 import { v4 as uuid } from 'uuid';
 import { IMatchweek } from '../../../features/schedules/types';
+import dayjs from 'dayjs';
 
 
 interface IMatchweekPickerProps {
   matchweeks: IMatchweek[];
   setMatchweek: (mw: IMatchweek) => void;
 }
+
+const DialogContainer = styled(Grid)`
+  padding: 1em;
+`;
+
+const MatchweekBtn = styled(Button)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MatchweekName = styled(Typography)`
+  font-size: 1em;
+`;
+
+const MatchweekDate = styled(Typography)`
+  font-size: .7em;
+`;
+
 
 const MatchweekPicker: React.FC<IMatchweekPickerProps> = ({ matchweeks, setMatchweek }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -28,16 +47,16 @@ const MatchweekPicker: React.FC<IMatchweekPickerProps> = ({ matchweeks, setMatch
         <Icon component={CalendarMonth} />
       </IconButton>
       <Dialog onClose={handlePickerOpen} open={isOpen}>
-        <Grid container spacing={3}>
+        <DialogContainer container spacing={3}>
           {matchweeks.map(mw => (
             <Grid key={uuid()} item xs={3}>
-              <Button onClick={() => handleSetMatchweek(mw)}>
-                <Typography>{mw.matchweekName}</Typography>
-                <Typography>{mw.basicDate}</Typography>
-              </Button>
+              <MatchweekBtn onClick={() => handleSetMatchweek(mw)}>
+                <MatchweekName>{mw.matchweekName}</MatchweekName>
+                <MatchweekDate>{dayjs(mw.basicDate).format('DD/MM/YYYY')}</MatchweekDate>
+              </MatchweekBtn>
             </Grid>
           ))}
-        </Grid>
+        </DialogContainer>
       </Dialog>
     </Box>
   );
