946d3865c6197338f2278eea824443192245ef5d
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-01 16:14:23 +0300

    Added tests for the CompetitionTeams component

---
+++ src/app/components/competitions/__tests__/CompetitionTeams.test.tsx
@@ -0,0 +1,54 @@
+import { cleanup, screen } from '@testing-library/react';
+import { setupCompetitionsSuccessHandlers } from '../../../utils/testing/serverMocks/competitions';
+import { renderWithProviders } from '../../../utils/testing/customRenderMethod';
+import { competitionToUpdate, competitionsStateSuccessMock } from '../../../utils/testing/testDataMocks/competitions';
+import { setupSchedulesSuccessHandlers } from '../../../utils/testing/serverMocks/schedules';
+import CompetitionTeams from '../CompetitionTeams';
+
+
+describe('CompetitionTeams tests', () => {
+  beforeEach(() => {
+    setupCompetitionsSuccessHandlers();
+    setupSchedulesSuccessHandlers();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should render the teams list', async () => {
+    renderWithProviders(
+      <CompetitionTeams />,
+      {
+        preloadedState: {
+          competitions: {
+            ...competitionsStateSuccessMock,
+            data: {
+              ...competitionsStateSuccessMock.data,
+              competition: competitionToUpdate
+            }
+          }
+        }
+      }  
+    );
+    expect(screen.getAllByRole('img')).toHaveLength(competitionToUpdate.clubs.length);
+  });
+
+  test('should render the loader component', async () => {
+    renderWithProviders(
+      <CompetitionTeams />,
+      {
+        preloadedState: {
+          competitions: {
+            ...competitionsStateSuccessMock,
+            data: {
+              ...competitionsStateSuccessMock.data,
+              competition: null
+            }
+          }
+        }
+      }  
+    );
+    expect(screen.getByTestId('backgroundLoader')).toBeInTheDocument();
+  });
+});
