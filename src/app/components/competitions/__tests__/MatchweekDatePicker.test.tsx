7f4176ae2b59a4fc913c250d5caaa0fe80548eee
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-01 16:32:09 +0300

    Added tests for the MatchweekDatePicker component

---
+++ src/app/components/competitions/__tests__/MatchweekDatePicker.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { scheduleToUpdate } from '../../../utils/testing/testDataMocks/schedules';
+import MatchweekPicker from '../MatchweekPicker';
+
+
+const propsMock = {
+  season: '2022/2023',
+  matchweeks: scheduleToUpdate.fixture,
+  setMatchweek: jest.fn()
+}
+
+describe('MatchweekDatePicker tests', () => {
+  test('should call the handler function after picking the matchweek', async () => {
+    render(
+      <MatchweekPicker 
+        season={propsMock.season} 
+        matchweeks={propsMock.matchweeks} 
+        setMatchweek={propsMock.setMatchweek} 
+      />
+    );
+    
+    const calendarBtn = screen.getByTestId('CalendarMonthIcon');
+    fireEvent.click(calendarBtn);
+
+    const matchweekBtn = screen.getByText('Matchweek 1');
+    fireEvent.click(matchweekBtn);
+
+    expect(propsMock.setMatchweek).toHaveBeenCalled();
+  });
+});
