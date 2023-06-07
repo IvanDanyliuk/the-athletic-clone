f8d4659d82c27c0df72d9f2cbbdc056e2e3db35b
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-07 15:04:32 +0300

    Added the PlayersTable component

--- src/app/components/clubs/ClubRoster.tsx
+++ src/app/components/clubs/ClubRoster.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box } from '@mui/material';
 
+import { AppDispatch } from '../../../features/store';
+import { selectClub } from '../../../features/clubs/selectors';
+import { selectAllPlayers } from '../../../features/players/selectors';
+import { getPlayers } from '../../../features/players/asyncActions';
+import PlayersTable from './PlayersTable';
+import { IPlayer } from '../../../features/players/types';
+
+
+interface IRoster {
+  goalkeepers: IPlayer[];
+  defenders: IPlayer[];
+  midfielders: IPlayer[];
+  attack: IPlayer[];
+}
 
 const ClubRoster: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const club = useSelector(selectClub);
+  const players = useSelector(selectAllPlayers);
+
+  const [roster, setRoster] = useState<IRoster>({
+    goalkeepers: [],
+    defenders: [],
+    midfielders: [],
+    attack: []
+  });
+
+  useEffect(() => {
+    if(players.length === 0) {
+      dispatch(getPlayers({ filterData: { club: club?.commonName } }));
+    }
+  }, [dispatch, club]);
+
+  useEffect(() => {
+    const goalkeepers = players.filter(player => player.position === 'GK');
+    const defenders = players.filter(player => player.position === 'D');
+    const midfielders = players.filter(player => player.position === 'M');
+    const attack = players.filter(player => player.position === 'A');
+    setRoster({ goalkeepers, defenders, midfielders, attack });
+  }, [players]);
+
   return (
-    <div>ClubRoster</div>
+    <Box>
+      <PlayersTable title='Goalkeepers' players={roster.goalkeepers} />
+      <PlayersTable title='Defenders' players={roster.defenders} />
+      <PlayersTable title='Midfielders' players={roster.midfielders} />
+      <PlayersTable title='Attack' players={roster.attack} />
+    </Box>
   );
 };
 
---
+++ src/app/components/clubs/PlayersTable.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { 
+  Avatar, Paper, Table, TableBody, TableCell, 
+  TableHead, TableRow, Typography, styled 
+} from '@mui/material';
+import dayjs from 'dayjs';
+import { v4 as uuid } from 'uuid';
+import { IPlayer } from '../../../features/players/types';
+import { Link } from 'react-router-dom';
+
+
+interface IPlayersTableProps {
+  title: string;
+  players: IPlayer[];
+}
+
+const Container = styled(Paper)`
+  margin-top: 2em;
+  padding: 1em;
+  max-width: 100%;
+  overflow: auto;
+`;
+
+const NameCell = styled(TableCell)`
+  width: 30em;
+`;
+
+const CountryCell = styled(TableCell)`
+  width: 20em;
+`;
+
+const PlayerLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #000000;
+  p {
+    margin-left: 1em;
+    font-weight: 500;
+  }
+`;
+
+const PlayersTable: React.FC<IPlayersTableProps> = ({ title, players }) => {
+  return (
+    <Container>
+      <Typography variant='h2_custom'>{title}</Typography>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            <NameCell>Name</NameCell>
+            <TableCell>POS</TableCell>
+            <TableCell>DOB</TableCell>
+            <TableCell>NUM</TableCell>
+            <CountryCell>NAT</CountryCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {players.map(player => (
+            <TableRow key={uuid()} hover>
+              <NameCell>
+                <PlayerLink to={`/players/${player._id}`}>
+                  <Avatar src={player.photoUrl} />
+                  <Typography>{`${player.firstName} ${player.lastName}`}</Typography>
+                </PlayerLink>
+              </NameCell>
+              <TableCell>{player.position}</TableCell>
+              <TableCell>{dayjs(player.birthDate).format('DD/MM/YYYY')}</TableCell>
+              <TableCell>{player.number}</TableCell>
+              <CountryCell>{player.country}</CountryCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
+
+export default PlayersTable;
