963d33d16c97ae07d8e210c62d496b909c6e549b
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-15 17:51:58 +0300

    Added divider for the filter lists

--- src/app/components/search/SearchFilters.tsx
+++ src/app/components/search/SearchFilters.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Box, Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, styled } from '@mui/material';
+import { 
+  Box, Checkbox, Divider, List, ListItem, ListItemButton, 
+  ListItemIcon, ListItemText, Typography, styled 
+} from '@mui/material';
 import { v4 as uuid} from 'uuid';
 import { ICompetition } from '../../../features/competitions/types';
-
 
 
 interface ISearchFiltersProps {
@@ -20,6 +22,14 @@ const FilterListItem = styled(ListItem)`
   padding: 0;
 `;
 
+const FilterListItemButton = styled(ListItemButton)`
+  padding: .5em 0;
+`;
+
+const ListDivider = styled(Divider)`
+  margin: 1em 0;
+`;
+
 
 const SearchFilters: React.FC<ISearchFiltersProps> = ({ authors, leagues, checkedLabels, onSetFilterData }) => {
   return (
@@ -27,28 +37,29 @@ const SearchFilters: React.FC<ISearchFiltersProps> = ({ authors, leagues, checke
       <List subheader={<Typography variant='h2_custom'>Authors</Typography>}>
         {authors.map(author => (
           <FilterListItem key={uuid()}>
-            <ListItemButton role={undefined} onClick={() => onSetFilterData(author)} dense>
+            <FilterListItemButton role={undefined} onClick={() => onSetFilterData(author)} dense>
               <ListItemText id={author} primary={author} />
               <ListItemIcon>
                 <Checkbox 
                   checked={checkedLabels.includes(author)} 
                 />
               </ListItemIcon>
-            </ListItemButton>
+            </FilterListItemButton>
           </FilterListItem>
         ))}
       </List>
+      <ListDivider orientation='horizontal' flexItem />
       <List subheader={<Typography variant='h2_custom'>Leagues</Typography>}>
         {leagues.map(league => (
           <FilterListItem key={uuid()}>
-            <ListItemButton role={undefined} onClick={() => onSetFilterData(league.fullName)} dense>
+            <FilterListItemButton role={undefined} onClick={() => onSetFilterData(league.fullName)} dense>
               <ListItemText id={league.fullName} primary={league.fullName} />
               <ListItemIcon>
                 <Checkbox 
                   checked={checkedLabels.includes(league.fullName)} 
                 />
               </ListItemIcon>
-            </ListItemButton>
+            </FilterListItemButton>
           </FilterListItem>
         ))}
       </List>
