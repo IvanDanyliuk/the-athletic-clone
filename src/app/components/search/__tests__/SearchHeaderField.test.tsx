2397d4f6dfab5ab2dd09541770df7b36cd762400
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-16 18:06:48 +0300

    Added tests for the SearchHeaderField component

--- src/app/components/search/__tests__/SearchHeaderField.test.tsx
+++ src/app/components/search/__tests__/SearchHeaderField.test.tsx
@@ -1,20 +1,31 @@
-import { screen } from '@testing-library/react';
-import { SearchHeaderField } from '../';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { renderWithProviders } from '../../../utils/testing/customRenderMethod';
+import { SearchHeaderField } from '../';
 
 
-const mockedUseNavigate = jest.fn();
+const mockedUseDispatch = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-   ...jest.requireActual('react-router-dom') as any,
-  useNavigate: () => mockedUseNavigate,
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux') as any,
+  useDispatch: () => mockedUseDispatch,
 }));
 
 
 describe('SearchHeaderField tests', () => {
   test('should call a navigate hook after passing the search request value', async () => {
-    renderWithProviders(<SearchHeaderField />)
+    renderWithProviders(<SearchHeaderField />);
 
-    screen.debug(undefined, 300000)
+    const searchBtn = screen.getByTestId('SearchIcon');
+    fireEvent.click(searchBtn);
+
+    const searchField = screen.getByRole('textbox');
+    fireEvent.change(searchField, { target: { value: 'Arsenal' } });
+
+    const searchFormBtn = screen.getAllByTestId('SearchIcon');
+    fireEvent.click(searchFormBtn[1]);
+
+    await waitFor(() => {
+      expect(mockedUseDispatch).toHaveBeenCalled();
+    });
   });
 });
