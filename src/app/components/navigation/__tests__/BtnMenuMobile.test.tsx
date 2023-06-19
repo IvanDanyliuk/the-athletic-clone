db44a3e0fe7e740be3ed3349ecdd8c05dcb1854c
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-19 18:12:27 +0300

    Added tests for the BtnMenuMobile component

---
+++ src/app/components/navigation/__tests__/BtnMenuMobile.test.tsx
@@ -0,0 +1,34 @@
+import { screen, render, fireEvent, cleanup } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { BtnMenuMobile } from '../';
+import { competitionsStateSuccessMock } from '../../../utils/testing/testDataMocks/competitions';
+
+
+describe('BtnMenu tests', () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <BtnMenuMobile links={competitionsStateSuccessMock.data.main.competitions.slice(0, 1)} />
+      </BrowserRouter>
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('should open the menu after clickin the open button', () => {
+    const menuBtn = screen.getByRole('button');
+    fireEvent.click(menuBtn);
+    expect(screen.getByText('Log In')).toBeInTheDocument();
+  });
+
+  test('should close the menu after clickin the open button', () => {
+    const menuBtn = screen.getByRole('button');
+    fireEvent.click(menuBtn);
+
+    const drawer = screen.getByRole('presentation');
+    fireEvent.click(drawer);
+    expect(screen.getByText('Log In')).toBeInTheDocument();
+  });
+});
