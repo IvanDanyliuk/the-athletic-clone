24068ba41f0468de4de2e4d110f3698e238c8a86
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-06-22 14:26:55 +0300

    Added tests for the Profile page

---
+++ src/app/pages/__tests__/Profile.test.tsx
@@ -0,0 +1,73 @@
+import { cleanup, screen } from '@testing-library/react';
+import { Profile } from '../';
+import { renderWithProviders } from '../../utils/testing/customRenderMethod';
+import { userToUpdate, usersStateSuccessMock } from '../../utils/testing/testDataMocks/users';
+import { setupUsersSuccessHandlers } from '../../utils/testing/serverMocks/users';
+
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom') as any,
+  useNavigate: () => mockedUseNavigate,
+}));
+
+
+describe('Profile page tests', () => {
+  beforeEach(() => {
+    setupUsersSuccessHandlers();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+  
+  test('should render all the navigation links if the user\'s role is admin or author', () => {
+    renderWithProviders(
+      <Profile />,
+      {
+        preloadedState: {
+          users: {
+            ...usersStateSuccessMock,
+            user: userToUpdate
+          }
+        }
+      }
+    );
+    expect(screen.getAllByRole('link')).toHaveLength(2)
+  });
+
+  test('should render one link (My Profile) if the user\'s role is reader', () => {
+    renderWithProviders(
+      <Profile />,
+      {
+        preloadedState: {
+          users: {
+            ...usersStateSuccessMock,
+            user: {
+              ...userToUpdate,
+              role: 'reader'
+            }
+          }
+        }
+      }
+    );
+    screen.debug(undefined, 300000);
+    expect(screen.getAllByRole('link')).toHaveLength(1)
+  });
+
+  test('should call the useNavigate hook if the use is not authorized', () => {
+    renderWithProviders(
+      <Profile />,
+      {
+        preloadedState: {
+          users: {
+            ...usersStateSuccessMock,
+            user: null
+          }
+        }
+      }
+    );
+    expect(mockedUseNavigate).toHaveBeenCalled();
+  });
+});
